80a8bd64ed3ed361ac4014794c7d202cf109f8e3
RohitBorakhade <borakhade03@gmail.com>
2025-11-16 23:58:56 +0530

    Create constants.js for scientific constants
    Added a module for scientific constants.

---
+++ modules/constants.js
@@ -0,0 +1,9 @@
+// constants.js - a small scientific constants panel (can be used later)
+export const CONSTANTS = {
+  pi: Math.PI,
+  e: Math.E,
+  g: 9.80665,
+  c: 299792458,
+  h: 6.62607015e-34,
+  R: 8.314462618
+};
